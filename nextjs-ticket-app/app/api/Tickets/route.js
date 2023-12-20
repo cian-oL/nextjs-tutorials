85f43d15bf020e58e9f428f45520f941b7067165
mecastor <mecastor87@gmail.com>
2023-12-20 13:54:30 +0000

    Replaced hard code with get functionality

--- nextjs-ticket-app/app/api/Tickets/route.js
+++ nextjs-ticket-app/app/api/Tickets/route.js
@@ -1,12 +1,23 @@
 import { NextResponse } from "next/server";
 import Ticket from "../../(models)/Ticket";
 
+// Create ticket
 export const POST = async (req) => {
   try {
     const body = await req.json();
     const ticketData = body.formData;
     await Ticket.create(ticketData);
     return NextResponse.json({ message: "Ticket created" }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ message: "Error", error }, { status: 500 });
+  }
+};
+
+// Get tickets
+export const GET = async (req) => {
+  try {
+    const tickets = await Ticket.find();
+    return NextResponse.json({ tickets }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: "Error", error }, { status: 500 });
   }
--- nextjs-ticket-app/app/page.jsx
+++ nextjs-ticket-app/app/page.jsx
@@ -1,13 +1,46 @@
 import TicketCard from "./(components)/TicketCard";
 
-const Dashboard = () => {
+const getTickets = async () => {
+  try {
+    // use full url because you are calling the server, not frontend
+    const response = await fetch("http://localhost:3000/api/Tickets", {
+      cache: "no-store", // don't store data, just check for new data
+    });
+
+    return response.json();
+  } catch (error) {
+    console.log("Failed to get tickets", error);
+  }
+};
+
+const Dashboard = async () => {
+  const { tickets } = await getTickets();
+
+  // map over tickets & whenever new ticket, it wil return w/o duplicates
+  const ticketCategories = [
+    ...new Set(tickets?.map(({ category }) => category)),
+  ];
+
   return (
     <div className="p-5">
-      <div className="lg:grid grid-cols-2 xl:grid-cols-4">
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
-        <TicketCard />
+      <div>
+        {tickets &&
+          ticketCategories?.map((category, categoryIndex) => (
+            <div key={categoryIndex} className="mb-4">
+              <h2>{category}</h2>
+              <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+                {tickets
+                  .filter((ticket) => ticket.category === category)
+                  .map((filteredTicket, _index) => (
+                    <TicketCard
+                      id={_index}
+                      key={_index}
+                      ticket={filteredTicket}
+                    />
+                  ))}
+              </div>
+            </div>
+          ))}
       </div>
     </div>
   );
